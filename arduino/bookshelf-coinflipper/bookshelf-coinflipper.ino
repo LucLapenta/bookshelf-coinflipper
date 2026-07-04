629c8a7e4328d072d87565e4b5b1958a79b9b645
Luc Lapenta <lapentaluc@gmail.com>
2026-07-04 15:23:49 -0600

    setup basic arduino sketch

--- arduino/bookshelf-coinflipper/bookshelf-coinflipper.ino
+++ arduino/bookshelf-coinflipper/bookshelf-coinflipper.ino
@@ -1,12 +1,35 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-  // Initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed."));
+    for(;;); // failed init, loop forever
+  }
+
+  display.clearDisplay();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println(F("Press to Flip"));
+  display.display();
+  delay(500);
+  
+
 }
